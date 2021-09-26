b392881967dd37bd2ebf4f4cd71555550df45ce1
Umera <umeramalek796@gmail.com>
2021-09-26 03:41:15 -0700

    added questions to the js

---
+++ asset/js/questions.js
@@ -0,0 +1,113 @@
+// creating an array of questions with correct answer and choices 
+let questions = [ {
+    Number:1,
+    question: "How many continents are there?",
+    answer: "b: 7",
+    options: [
+        "a: 8",
+        "b: 7",
+        "c: 6",
+        "d: 5",
+    ]
+},
+{
+    Number:2,
+    question:"Which is the largest continent?",
+    answer: "a: Asia",
+    options: [
+        "a: Asia",
+        "b: Antartica",
+        "c: China",
+        "d: America",
+    ]
+},
+{
+    Number:3,
+    question:"Who is the founder of Facebook?",
+    answer: "d: Mark Zuckerberg",
+    options: [
+        "a: Elon Musk",
+        "b: Kim Kardasian",
+        "c: Steve Jobs",
+        "d: Mark Zuckerberg",
+    ]
+},
+{
+    Number:4,
+    question:"Which country has the highest population?",
+    answer: "c: China",
+    options: [
+        "a: India",
+        "b: North America",
+        "c: China",
+        "d: Russia",
+    ]
+},
+{
+    Number:5,
+    question:"Which planet is closest to planet Earth?",
+    answer: "a: Venus",
+    options: [
+        "a: Venus",
+        "b: Mars",
+        "c: Moon",
+        "d: Pluto",
+    ]
+},
+{
+    Number:6,
+    question: "Which is the tallest mountain in the mountain?",
+    answer:"d: Mount Everest",
+    options: [
+        "a: K2",
+        "b: Kanchenjunga",
+        "c: Lhotse",
+        "d: Mount Everest",
+    ]
+},
+{
+    Number:7,
+    question: "What is the name of the longest ocean on Earth?",
+    answer: "c: Pacific Ocean",
+    options: [
+        "a: Atlantic Ocean",
+        "b: Indian Ocean",
+        "c: Pacific Ocean",
+        "d: Arctic Ocean",
+    ]
+},
+{
+    Number:8,
+    question: "Which is the biggest desert in the world",
+    answer:"b: Sahara Desert",
+    options: [
+        "a: Namib Desert",
+        "b: Sahara Desert",
+        "c: Gobi Desert",
+        "d: Arabian Desert",
+    ]
+},
+{
+    Number:9,
+    question: "What is the capital of Canada?",
+    answer: "a: Ottawa",
+    options: [
+        "a: Ottawa",
+        "b: Toronto",
+        "c: Montreal",
+        "d: Vancouver",
+    ]
+},
+{
+    Number:10,
+    question: "Which is the coldest place on Earth?",
+    answer: "b: Antartica",
+    options: [
+        "a: Iceland",
+        "b: Antartica",
+        "c: Canada",
+        "d: London",
+    ]
+},
+
+]
