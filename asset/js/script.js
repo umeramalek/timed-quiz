655ea73ac88f6ba534f0fc3b78ecf6e8ace86f7f
Umera <umeramalek796@gmail.com>
2021-09-27 23:02:40 -0700

    in js added start button function

--- asset/js/script.js
+++ asset/js/script.js
@@ -14,4 +14,27 @@ const submit_btn = document.querySelector("submitBtn");
 
 const highScoreBox = document.querySelector("highscoresBox");
 const restart_btn = document.querySelector("restart_button");
+const endpage = document.querySelector("restart_box");
 
+// setting initial score, questions and seconds
+let questions = 0;
+let score = 0;
+let seconds = 120;
+
+// when start button is clicked go to the questions and start the timer
+start_btn.addEventListener("click", function () {
+    let timerInterval = setInterval(timerFunc, 1000);
+    function timerFunc() {
+      secondsLeft = secondsLeft - 1;
+      timerBox.textContent = secondsLeft + "s";
+      if (secondsLeft <= 0) {
+        document.getElementById('timeout').style.display = "block";
+        setTimeout(function(){
+          document.getElementById('timeout').style.display = "none";
+        }, 1000)
+        endpage();
+        clearInterval(timerInterval);
+        return;
+    }
+}
+}
