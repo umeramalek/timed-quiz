02a9d4a000852c4e2013d689972ce8229c51e82e
Umera <umeramalek796@gmail.com>
2021-09-27 21:33:57 -0700

    in js added all the required elements from html

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,3 +1,17 @@
 // all the required elements
 const start_btn = document.querySelector("#startButton");
-const 
+const highscore_btn = document.querySelector("high_scores");
+const timerBox = document.querySelector("timer");
+const questionsAns = document.querySelector("questionsAnswered");
+const questionsBox = document.querySelector("questions_box");
+const optionsBox = document.querySelector("options_list");
+const scoreBox = document.querySelector("score");
+const correctlyAns = document.querySelector("correctlyAnswered");
+
+const scoreNameBox = document.querySelector("scoreNameContainer");
+const scoreName = document.querySelector("scoreName");
+const submit_btn = document.querySelector("submitBtn");
+
+const highScoreBox = document.querySelector("highscoresBox");
+const restart_btn = document.querySelector("restart_button");
+
