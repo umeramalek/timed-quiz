174f3d83009b0dbb6740bff444737fdb573bfcb8
Umera <umeramalek796@gmail.com>
2021-09-27 20:32:50 -0700

    fixed the structure for html and css

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,14 +1,3 @@
 // all the required elements
 const start_btn = document.querySelector("#startButton");
-
-
-
-
-
-
-// function for starting the quiz
-function startQuiz() {
-    
-}
-// when start button clicked, start the quiz 
-start_btn.addEventListener("click", startQuiz);
+const 
